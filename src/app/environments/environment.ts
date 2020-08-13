eae0d05ffcc5159167d6afff864a628717f8a63c
ns484een <59837719+m-nishide@users.noreply.github.com>
2020-08-13 15:59:07 +0900

    Create environment.ts

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,9 @@
+import { OpenWeatherMapService } from './../app/services/open-weather-map.service';
+
+export const environment = {
+  production: false,
+  openWeatherMap: {
+    api: 'xxxxxxxxxxxxxxxxxx',
+    apiKey: 'xxxxxxxxxxxxxxxxxx',
+  },
+};
